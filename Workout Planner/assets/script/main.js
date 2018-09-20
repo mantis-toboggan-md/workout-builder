784e65d6b2e0ac7994dde209d638949b5b2dc917
Nancy <mantis-toboggan-md@users.noreply.github.com>
2018-09-20 15:50:14 -0700

    added exercise selection form

--- Workout Planner/assets/script/main.js
+++ Workout Planner/assets/script/main.js
@@ -14,14 +14,29 @@ $(document).ready(()=>{
       }
   ];
   for(var i = 0; i < exercises.length; i++){
-    $("#exerciseFormSelect").replace("<option>" + exercises[i].name + "</option>");
+    $("#exerciseFormSelect").append("<option>" + exercises[i].name + "</option>");
     console.log(exercises[i].name);
   };
+$("#exerciseFormSelect").select2();
 
-  // 
-  // document.getElementById("exerciseFormSelect").oninput = function(){
-  //   var exerciseSelect = document.getElementById("exerciseFormSelect").value;
-  //   $("#exerciseListSun").append('<li>' + exerciseSelect + '</li>');
-  // }
+  document.getElementById("addExButton").onclick = function(){
+    var exerciseSelect = document.getElementById("exerciseFormSelect").value;
+    var sets = document.getElementById("setsText").value;
+    var reps = document.getElementById("repsText").value;
+    $("#exerciseListSun").append(  '<li>' +
+      '<input type="button" class="btn btn-danger" value = "x"></input>' +
+      exerciseSelect +
+      " " +
+      sets +
+      " x " +
+      reps +
+      '</li>'
+    );
+    console.log("button pressed")
+  };
+  $(".btn btn-danger").onclick = function(){
+    $(this).parent().remove();
+    console.log("button pressed");
+  };
 
 });
